ae1c4618f36de082f98a43dba4b6a64c087f2e4d
yank5 <yank5@nycstudents.net>
2022-10-19 12:34:18 -0400

    c

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 import java.util.Scanner;
-import java.lang.Math;
 public class Main {
     public static void main(String[] args) {
-
-
-
+        LinearEquation obj = new LinearEquation();
         Scanner ab = new Scanner(System.in);
         System.out.print("first coordinate(separate with comma): ");
         String test1 = ab.nextLine();
@@ -28,45 +25,13 @@ public class Main {
         int d3 = Integer.parseInt(f3.strip());
         int d4 = Integer.parseInt(f4.strip());
 
-        System.out.println(d1+" "+d2);
-        System.out.println(d3+" "+d4);
+        obj.test(d1,d2,d3,d4);
 
-        System.out.println("points: ("+d1+", "+d2+") "+"and ("+d3+", "+d4+")");
-        //slope
+        System.out.println(obj.lineInfo());
 
-        double slope = (d4-d2)/(d3-d1);
-
-        System.out.println("slope of line " +slope);
-
-        double yint;
-
-        if(slope*d1!=d2){
-            if(slope*d1>d2){
-                yint = d2-slope*d1;
-            } else {
-                yint = slope*d1-d2;
-            }
-        } else{
-            yint = 0;
-        }
-
-        char DD;
-        if(yint>=0){
-            DD = '+';
-        } else {
-            DD = '-';
-        }
-
-        System.out.println("equation of line: "+slope+"x"+DD+yint);
-
-        System.out.println("y intercept: "+yint);
-
-        double dist = Math.sqrt(Math.pow(d3-d1,2)+Math.pow(d4-d2,2));
-        System.out.println("distance between: " +dist);
 
         System.out.print("enter an x value: ");
         Double cc = ab.nextDouble();
 
-        System.out.println("\nthe point on the line is: ("+cc+", "+slope*cc+")");
     }
 }
